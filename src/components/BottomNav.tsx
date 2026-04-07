45cb42179265848a8c999a4479b172291b9c4cf6
Rory <roryoisinlynch@gmail.com>
2026-04-07 18:43:56 +0100

    test

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -81,18 +81,17 @@ export default function BottomNav({ activeTab }: BottomNavProps) {
         <span className="bottom-nav__icon" aria-hidden="true">
           <svg viewBox="0 0 24 24" className="bottom-nav__svg">
             <path
-              d="M12 9.25a2.75 2.75 0 1 0 0 5.5 2.75 2.75 0 0 0 0-5.5Z"
+              d="M12 9.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Z"
               fill="none"
               stroke="currentColor"
               strokeWidth="1.8"
             />
             <path
-              d="M19.15 12a1.2 1.2 0 0 0 .03-.28 1.2 1.2 0 0 0-.03-.28l1.73-1.35a.45.45 0 0 0 .11-.58l-1.64-2.84a.45.45 0 0 0-.54-.2l-2.04.82a6.9 6.9 0 0 0-.97-.56l-.31-2.17a.45.45 0 0 0-.44-.38h-3.28a.45.45 0 0 0-.44.38l-.31 2.17c-.34.14-.67.33-.97.56l-2.04-.82a.45.45 0 0 0-.54.2L3.01 9.5a.45.45 0 0 0 .11.58l1.73 1.35a1.2 1.2 0 0 0-.03.28c0 .09.01.19.03.28l-1.73 1.35a.45.45 0 0 0-.11.58l1.64 2.84a.45.45 0 0 0 .54.2l2.04-.82c.3.23.63.42.97.56l.31 2.17a.45.45 0 0 0 .44.38h3.28a.45.45 0 0 0 .44-.38l.31-2.17c.34-.14.67-.33.97-.56l2.04.82a.45.45 0 0 0 .54-.2l1.64-2.84a.45.45 0 0 0-.11-.58L19.15 12Z"
+              d="M12 3.5v2M12 18.5v2M18.5 12h2M3.5 12h2M17 7l1.4-1.4M5.6 18.4 7 17M17 17l1.4 1.4M5.6 5.6 7 7"
               fill="none"
               stroke="currentColor"
               strokeWidth="1.8"
               strokeLinecap="round"
-              strokeLinejoin="round"
             />
           </svg>
         </span>
